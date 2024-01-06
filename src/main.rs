6a053b8b90e1baf9bb723bde643b4ed181616756
ReDestroyDeR <daniil.schroder@gmail.com>
2024-01-06 17:51:12 +0300

    Simple Character movement + idle animation

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ mod player;
 use crate::motd::MotdPlugin;
 use bevy::prelude::*;
 use bevy_ecs_tilemap::TilemapPlugin;
-use player::PlayerPlugin;
+use player::{PlayerPlugin, PlayerBundle, AnimationIndices, AnimationTimer};
 
 fn main() {
     App::new()
@@ -28,13 +28,34 @@ fn main() {
         .run();
 }
 
-fn startup(mut commands: Commands, asset_server: Res<AssetServer>) {
+fn startup(mut commands: Commands, asset_server: Res<AssetServer>, mut texture_atlases: ResMut<Assets<TextureAtlas>>,) {
     commands.spawn(Camera2dBundle::default()); 
 
     let map_handle: Handle<helpers::tiled::TiledMap> = asset_server.load("levels/level1.tmx");
 
     commands.spawn(helpers::tiled::TiledMapBundle {
         tiled_map: map_handle,
+        ..default()
+    });
+
+    let archer = asset_server.load("sprites/Factions/Knights/Troops/Archer/Blue/Archer_Blue.png");
+    let atlas = 
+        TextureAtlas::from_grid(archer, Vec2::new(192., 192.), 8, 7, None,  None);
+    let texture_atlas_handle = texture_atlases.add(atlas);
+
+    let animation_indices = AnimationIndices::Bounded { first: 0, last: 5 };
+    commands.spawn(PlayerBundle {
+        sprite: SpriteSheetBundle  {
+            texture_atlas: texture_atlas_handle,
+            sprite: match animation_indices {
+                AnimationIndices::Bounded{first, last: _} => TextureAtlasSprite::new(first),
+                _ => TextureAtlasSprite::new(0)
+            },
+            transform: Transform::from_xyz(0., 0., 100.),
+            ..default()
+        },
+        animation_indices,
+        animation_timer: AnimationTimer(Timer::from_seconds(0.1, TimerMode::Repeating)),
         ..default()
     });
 }
--- src/player.rs
+++ src/player.rs
@@ -41,12 +41,26 @@ impl Default for Health {
 #[derive(Default, Component)]
 pub struct Player;
 
+#[derive(Default, Component)]
+pub enum AnimationIndices {
+    #[default]
+    NoAnimation,
+    Bounded{
+        first: usize,
+        last: usize,
+    }
+}
+
+#[derive(Default, Component, Deref, DerefMut)]
+pub struct AnimationTimer(pub Timer);
+
 #[derive(Default, Bundle)]
 pub struct PlayerBundle {
-    _marker: Player,
-    health: Health,
-    sprite: SpriteBundle,
-    transform: Transform
+    pub _marker: Player,
+    pub health: Health,
+    pub sprite: SpriteSheetBundle,
+    pub animation_indices: AnimationIndices,
+    pub animation_timer: AnimationTimer,
 }
 
 pub struct PlayerPlugin;
@@ -54,9 +68,31 @@ pub struct PlayerPlugin;
 impl Plugin for PlayerPlugin {
 
     fn build(&self, app: &mut App) {
-        app.add_systems(Update, movement);
+        app.add_systems(Update, (movement, animate_sprite));
     }
 
+}
+
+fn animate_sprite(
+    time: Res<Time>,
+    mut query: Query<(
+        &AnimationIndices,
+        &mut AnimationTimer,
+        &mut TextureAtlasSprite,
+    )>,
+) {
+    for (indices, mut timer, mut sprite) in &mut query {
+        if let AnimationIndices::Bounded{first, last} = indices {
+            timer.tick(time.delta());
+            if timer.just_finished() {
+                sprite.index = if sprite.index == *last {
+                    *first
+                } else {
+                    sprite.index + 1
+                };
+            }
+        }
+    }
 }
 
 fn movement(
@@ -67,48 +103,61 @@ fn movement(
     mut camera: Query<(&mut Transform, &mut OrthographicProjection), With<Camera>>,
 ) {
     // Camera
-    for (mut transform, mut ortho) in camera.iter_mut() {
-        let mut direction = Vec3::ZERO;
+    for (mut camera_transform, mut ortho) in camera.iter_mut() {
+        for mut player_transform in player.iter_mut() {
+            zoom_handler(&mut mouse_input, &mut ortho);
 
-        if keyboard_input.pressed(KeyCode::A) {
-            direction -= Vec3::new(1.0, 0.0, 0.0);
+            let z = player_transform.translation.z;
+            player_transform.translation += time.delta_seconds() * direction_from(&keyboard_input) * 500.;
+            // Important! We need to restore the Z values when moving the camera around.
+            // Bevy has a specific camera setup and this can mess with how our layers are shown.
+            player_transform.translation.z = z;
+
+            // For now just follow eagerly
+            camera_transform.translation = player_transform.translation;
         }
-
-        if keyboard_input.pressed(KeyCode::D) {
-            direction += Vec3::new(1.0, 0.0, 0.0);
-        }
-
-        if keyboard_input.pressed(KeyCode::W) {
-            direction += Vec3::new(0.0, 1.0, 0.0);
-        }
-
-        if keyboard_input.pressed(KeyCode::S) {
-            direction -= Vec3::new(0.0, 1.0, 0.0);
-        }
-
-        for ev in mouse_input.iter() {
-            if ev.y > 0. {
-                ortho.scale += (ev.y / 10.).max(0.1);
-            } else if ev.y < 0. {
-                ortho.scale += (ev.y / 10.).min(-0.1);
-            }
-        }
-
-        if ortho.scale > 1.5 {
-            ortho.scale = 1.5;
-        }
-
-        if ortho.scale < 0.5 {
-            ortho.scale = 0.5;
-        }
-
-        let z = transform.translation.z;
-        transform.translation += time.delta_seconds() * direction * 500.;
-        // Important! We need to restore the Z values when moving the camera around.
-        // Bevy has a specific camera setup and this can mess with how our layers are shown.
-        transform.translation.z = z;
     }
 
+}
+
+fn direction_from(keyboard_input: &Res<'_, Input<KeyCode>>) -> Vec3 {
+    let mut direction = Vec3::ZERO;
+
+    if keyboard_input.pressed(KeyCode::A) {
+        direction -= Vec3::new(1.0, 0.0, 0.0);
+    }
+
+    if keyboard_input.pressed(KeyCode::D) {
+        direction += Vec3::new(1.0, 0.0, 0.0);
+    }
+
+    if keyboard_input.pressed(KeyCode::W) {
+        direction += Vec3::new(0.0, 1.0, 0.0);
+    }
+
+    if keyboard_input.pressed(KeyCode::S) {
+        direction -= Vec3::new(0.0, 1.0, 0.0);
+    }
+    
+    direction
+}
+
+fn zoom_handler(mouse_input: &mut EventReader<'_, '_, MouseWheel>, ortho: &mut Mut<'_, OrthographicProjection>) {
+    for ev in mouse_input.iter() {
+        if ev.y > 0. {
+            ortho.scale += (ev.y / 10.).max(0.1);
+        } else if ev.y < 0. {
+            ortho.scale += (ev.y / 10.).min(-0.1);
+        }
+    }
+
+    if ortho.scale > 1.5 {
+        ortho.scale = 1.5;
+    }
+
+    if ortho.scale < 0.5 {
+        ortho.scale = 0.5;
+    }
 }
 
 
